db7581efa19ae16eb15fd9fa4b662240f5f52542
xiaolin <462441355@qq.com>
2019-07-18 18:28:50 +0800

    * 修改名称

---
+++ lib/mong/cfg.go
@@ -0,0 +1,16 @@
+package mong
+
+import "time"
+
+type Config struct {
+	Host       string        // 连接地址
+	Username   string        // 用户名
+	Password   string        // 密码
+	Database   string        // 数据库名称
+	Collection string        // 数据库集合名称
+	Timeout    time.Duration // 请求超时时间
+}
+
+func (c *Config) NewMgo() (*Mgo, error) {
+	return &Mgo{cfg: *c}, nil
+}
